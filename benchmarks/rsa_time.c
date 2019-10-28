6790f866216b4886fabd58b02932c707a4c0be0b
Jacob Michael Hage <jakehage@eecs583b.eecs.umich.edu>
2019-10-28 23:50:02 +0000

    fixed u_int64_t typo in rsa_time.c

--- benchmarks/rsa_time.c
+++ benchmarks/rsa_time.c
@@ -2,7 +2,6 @@
 #include <math.h>
 #include <stdlib.h>
 #include <time.h>
-#include <sys/time.h>
 
 void decimal_to_binary(int op1, int aOp[]){
     int result, i = 0;
@@ -28,11 +27,11 @@ int modular_exponentiation(int a, int b, int n){
   clock_gettime(CLOCK_MONOTONIC_RAW, &start);
   d = (d*d) % n;
   if (bb[i] == 1) {
-   d = (d*a) % n;
+    d = (d*a) % n;
   }
- clock_gettime(CLOCK_MONOTONIC_RAW, &end);
-  uint64_t delta_us = (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_nsec - start.tv_nsec) / 1000;
- printf("bit %d loop time %ld\n", bb[i], delta_us);
+  clock_gettime(CLOCK_MONOTONIC_RAW, &end);
+  u_int64_t delta_us = (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_nsec - start.tv_nsec) / 1000;
+  printf("bit %d loop time %ld\n", bb[i], delta_us);
  }
  return d;
 }
