721c4aca7aaac84563732549712cfdbc35526083
Jacob Michael Hage <jakehage@eecs583b.eecs.umich.edu>
2019-10-29 00:13:37 +0000

    it compiles!

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 Hi
 
+gcc -std=c99 rsa_time.c -o rsa_time -lm
+
--- benchmarks/rsa_time.c
+++ benchmarks/rsa_time.c
@@ -16,22 +16,23 @@ void decimal_to_binary(int op1, int aOp[]){
 int modular_exponentiation(int a, int b, int n){
  int *bb;
  int count = 0, d = 1, i;
- struct timespec end, start;
+ double delta_us;
+ clock_t start, end;
  // find out the size of binary b
  count = (int) (log(b)/log(2)) + 1;
- 
- bb = (int *) malloc(sizeof(int*) * count);
+
+ bb = (int ) malloc(sizeof(int) * count);
  decimal_to_binary(b, bb);
- 
+
  for (i = count - 1; i >= 0; i--) {
-  clock_gettime(CLOCK_MONOTONIC_RAW, &start);
+  start = clock();
   d = (d*d) % n;
   if (bb[i] == 1) {
-    d = (d*a) % n;
+   d = (d*a) % n;
   }
-  clock_gettime(CLOCK_MONOTONIC_RAW, &end);
-  u_int64_t delta_us = (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_nsec - start.tv_nsec) / 1000;
-  printf("bit %d loop time %ld\n", bb[i], delta_us);
+ end = clock();
+ delta_us = (((double)end - (double)start) / (double)CLOCKS_PER_SEC);
+ printf("bit %d loop time %f\n", bb[i], delta_us);
  }
  return d;
 }
