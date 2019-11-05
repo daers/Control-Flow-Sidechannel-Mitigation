b5b821c7adf6bd48193e7665d3c8276f48ea0a4a
cnoujaim <cnoujaim@eecs583a.eecs.umich.edu>
2019-11-05 01:11:00 +0000

    better primes

--- benchmarks/rsa.c
+++ benchmarks/rsa.c
@@ -16,17 +16,20 @@ void decimal_to_binary(int op1, int aOp[]){
 int modular_exponentiation(int a, int b, int n){
  int *bb;
  int count = 0, d = 1, i;
- 
+
  // find out the size of binary b
  count = (int) (log(b)/log(2)) + 1;
- 
+
  bb = (int *) malloc(sizeof(int*) * count);
  decimal_to_binary(b, bb);
- 
+
  for (i = count - 1; i >= 0; i--) {
   d = (d*d) % n;
   if (bb[i] == 1) {
-   d = (d*a) % n;
+      d = (d*a) % n;
+  }
+  else {
+      d = d;
   }
  }
  return d;
