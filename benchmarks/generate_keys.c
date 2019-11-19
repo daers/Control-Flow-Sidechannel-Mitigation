d417a75eb311d7191d41c22c0f43a64bc151708c
daers <darojas@umich.edu>
2019-11-18 19:26:01 -0500

    Adding script for generating keys, keys and changes to rsa_time_c11. Still need to move timing part.

--- benchmarks/generate_keys.c
+++ benchmarks/generate_keys.c
@@ -10,13 +10,13 @@
 int COUNT = 0;
 
 long int gcd(long int a, long int b){
-	if (b == 0)
-      return a;
-   return gcd(b, a % b);
+  if (b == 0)
+    return a;
+  return gcd(b, a % b);
 }
 
 long int lcm(long int a, long int b){
-	return (a*b)/gcd(a, b);
+  return (a*b)/gcd(a, b);
 }
 
 
@@ -26,8 +26,8 @@ long int gcdExtended(long int a, long int b, long int *x, long int *y)
   // Base Case
   if (a == 0)
   {
-	  *x = 0, *y = 1;
-	  return b;
+    *x = 0, *y = 1;
+    return b;
   }
 
   long int x1, y1; // To store results of recursive call
@@ -44,15 +44,17 @@ long int gcdExtended(long int a, long int b, long int *x, long int *y)
 // Function to find modulo inverse of a
 long int modInverse(long int a, long int m)
 {
-    long int x, y;
-    long int g = gcdExtended(a, m, &x, &y);
-    if (g != 1)
-        printf("Inverse doesn't exist");
-    else
-    {
+  long int x, y;
+  long int g = gcdExtended(a, m, &x, &y);
+  if (g != 1)
+    printf("Inverse doesn't exist");
+  else
+  {
         // m is added to handle negative x
-        return (x%m + m) % m;
-    }
+    return (x%m + m) % m;
+  }
+  
+  return -1;
 }
 
 
@@ -62,13 +64,13 @@ int main(){
   fileptr = fopen("modexp_data.txt", "w+");
 
   long int primes[32] = {314606891, 334214459, 817504253, 838041641, 334214467,353868013, 838041647, 858599503, 353868019, 373587883, 858599509, 879190747, 373587911, 393342739, 879190841, 899809343, 393342743, 413158511, 899809363, 920419813, 413158523, 433024223, 920419823, 941083981, 433024253, 452930459, 941083987, 961748927, 452930477, 472882027, 961748941, 982451653};
-  long int values[4][1000];
 
-  int countValues = 0;
+  int countVals = 0, i = 0;
   
-  for (int i = 0; i < 1000; ++i){
+  while(countVals < 1000){
     long int p = primes[rand() % 32];
     long int q = primes[rand() % 32];
+    i++;
 
     if(p == q){
       printf("P == Q. problem in iter %d. skipping\n", i);
@@ -84,18 +86,9 @@ int main(){
     long int m = rand();    
     long int d = modInverse(65537, lamda);
     
-    values[0][countVals] = m;
-    values[1][countVals] = d;
-    values[2][countVals] = p;
-    values[3][countVals] = q;
+    fprintf(fileptr, "%ld\t%ld\t%ld\t%ld\n", m, d, p, q);
 
     ++countVals;
-  }
-
-  fprintf(fileptr, "%d\n", countVals);
-
-  for (int i = 0; i < countVals; ++i){
-    fprintf(fileptr, "%ld\t%ld\t%ld\t%ld\n", values[0][i], values[1][i], values[2][i], values[3][i]);
   }
   
   fclose(fileptr);
--- benchmarks/rsa_time_c11.c
+++ benchmarks/rsa_time_c11.c
@@ -112,78 +112,29 @@ long int modular_exponentiation(long int a, long int b, long int n){
   return d;
 }
 
-long int gcd(long int a, long int b){
-	if (b == 0)
-      return a;
-   return gcd(b, a % b);
-}
-
-long int lcm(long int a, long int b){
-	return (a*b)/gcd(a, b);
-}
-
-
-// C function for extended Euclidean Algorithm
-long int gcdExtended(long int a, long int b, long int *x, long int *y)
-{
-  // Base Case
-  if (a == 0)
-  {
-	  *x = 0, *y = 1;
-	  return b;
-  }
-
-  long int x1, y1; // To store results of recursive call
-  long int gcd = gcdExtended(b%a, a, &x1, &y1);
-
-  // Update x and y using results of recursive
-  // call
-  *x = y1 - (b/a) * x1;
-  *y = x1;
-
-  return gcd;
-}
-// Function to find modulo inverse of a
-long int modInverse(long int a, long int m)
-{
-    long int x, y;
-    long int g = gcdExtended(a, m, &x, &y);
-    if (g != 1)
-        printf("Inverse doesn't exist");
-    else
-    {
-        // m is added to handle negative x
-        return (x%m + m) % m;
-    }
-}
-
-
 
 int main(){
-	long int a = modular_exponentiation(123452,765435,93093587);
-	long int primes[32] = {314606891, 334214459, 817504253, 838041641, 334214467,353868013, 838041647, 858599503, 353868019, 373587883, 858599509, 879190747, 373587911, 393342739, 879190841, 899809343, 393342743, 413158511, 899809363, 920419813, 413158523, 433024223, 920419823, 941083981, 433024253, 452930459, 941083987, 961748927, 452930477, 472882027, 961748941, 982451653};
+  FILE *fileptr;
+  int tempChar;
+  fileptr = fopen("modexp_data.txt", "r");
+  long int d, m, p, q;
+  // long int values[4][1000];
+
 	for (int i = 0; i < 1000; ++i){
-		long int p = primes[rand() % 32];
-		long int q = primes[rand() % 32];
-		if(p == q){
-			printf("P == Q. problem. skipping iter\n");
-			continue;
-		}
-		long int m = rand();
-		long int lamda = lcm(p-1, q-1);
-		if (gcd(lamda, 65537) != 1){
-			printf("You fucked up");
-		}
-		long int d = modInverse(65537, lamda);
-		printf("Our number: m = %ld, d = %ld, n = %ld\n", m, d, p*q);
+    // Read m, d, p, q from file
+		fscanf(fileptr, "%d", &d);
+    fscanf(fileptr, "%d", &m);
+    fscanf(fileptr, "%d", &p);
+		fscanf(fileptr, "%d", &q);
+
 		long int a = modular_exponentiation(m, d, p * q);
 	}
 
 	printf("\n\nPRINTING OVERALL STATS");
 	printf("\n\nAverage difference between 0 bit and 1 bit\n");
 	for (int i = 0; i < COUNT; ++i) {
-      printf("%f\n", STATS[1][i] - STATS[0][i]);
-    }
+    printf("%f\n", STATS[1][i] - STATS[0][i]);
+  }
 
-
+  fclose(fileptr);
 }
