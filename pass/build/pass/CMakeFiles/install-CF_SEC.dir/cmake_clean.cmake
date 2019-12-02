9de90cec38a3ff136efff7e07372bad0d57532ac
daers <darojas@umich.edu>
2019-12-01 23:22:42 -0500

    Gitignore is now working

--- pass/build/pass/CMakeFiles/install-CF_SEC.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/install-CF_SEC"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/install-CF_SEC.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
