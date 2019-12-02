c439913adca0e29e822b914839bfae437e8ed935
daers <darojas@umich.edu>
2019-12-01 23:19:34 -0500

    Cleaning up stats processing file.

--- benchmarks/data_processing.py
+++ benchmarks/data_processing.py
@@ -16,6 +16,13 @@ for filename in os.listdir(stats_directory):
 
 		statistics.append(data)
 
+print("\n")
+print("")
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+print("~ PRE AND POST PASS STATISTICS ~")
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+print("\n")
+
 for idx, df in enumerate(statistics):
 	print("Data for data frame " + str(idx))
 	print("Zero Average: " + str(df["zero_avg"].mean()))
@@ -26,32 +33,4 @@ for idx, df in enumerate(statistics):
 	print("\n")
 
 
-
-# class Stats_Info:
-# 	def __init__(self, exp, zero_avg, zero_stdev, one_avg, one_stdev, diff):
-# 	    self.exp = exp
-# 	    self.zero_avg = zero_avg
-# 	    self.zero_stdev = zero_stdev
-# 	    self.one_avg = one_avg
-# 	    self.one_stdev = one_stdev
-# 	    self.diff = diff
-
-# class File_Stats:
-# 	def __init__(self, name_in, data_in):
-# 		name = name_in
-# 		data = data_in.copy()
-
-# 	def append_stats(self, new_value):
-# 		self.stats_list.append(new_value)
-
-# for filename in os.listdir(stats_directory):
-# 	if filename.endswith(".csv"):
-# 		filename_complete = stats_directory + filename
-# 		with open(filename_complete, "r") as stats_file:
-# 			current_file = File_Stats()
-# 			readCSV = csv.reader(stats_file, delimiter='\t')
-# 			for row in readCSV:
-# 				row_stats = Stats_Info(row[0], row[1], row[2], row[3], row[4], row[5])
-# 				current_file.append_stats(row_stats)
-# 			statistics.append(current_file)
 
