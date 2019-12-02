877ade6bc3b7b36d5a14a23914db41d79359f0b0
daers <darojas@umich.edu>
2019-12-01 22:40:19 -0500

    Created master shell script. Do ./master_rum.sh and enjoy the ride.

---
+++ master_run.sh
@@ -0,0 +1,17 @@
+# To run, do: ./master_run.sh name_of_benchmark
+# You will need to pip install pandas and matplotlib
+BENCH=${1}
+
+# Gather data pre pass and post pass
+cd pass
+./run.sh ${BENCH}
+
+./no_pass.sh ${BENCH}
+
+cd ../benchmarks
+
+python3 data_processing.py
+
+cd ../
+
+git checkout pass/build/
